f125ad6d4e474426b22d16548a656b5a0c80b68f
QiwenDun <84956313+QiwenDun@users.noreply.github.com>
2022-01-23 16:04:16 +1100

    Add files via upload

---
+++ layout/scripts/toast.js
@@ -0,0 +1,4 @@
+document.querySelector('js').addEventListener('click', showMessage);
+$('button').click(fliterItems);
+  function showMessage() { alert('Welcome to Wave Fighters! Since we need to guarantee the quality of our members, if you are sure that you love surfing and are keen to take part in our activities, then click OK.'); }
+  
